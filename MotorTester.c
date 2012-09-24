422de2cac306492630086cbccc23a38a93ae71b0
Foster McLane <fkmclane@gmail.com>
2012-09-24 15:20:24 -0400

    Fixed sensor

--- MotorTester.c
+++ MotorTester.c
@@ -1,6 +1,5 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S3,     IR,             sensorAnalog)
+#pragma config(Sensor, S2,     IR,             sensorHiTechnicIRSeeker1200)
 #pragma config(Motor,  mtr_S1_C1_1,     RightForward,  tmotorTetrix, PIDControl, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     BackSideways,  tmotorTetrix, PIDControl, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     FrontSideways, tmotorTetrix, PIDControl, encoder)
