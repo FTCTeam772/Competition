70616f2d203c5e3d222a093d6f0f8876b35953da
Foster McLane <fkmclane@gmail.com>
2013-03-22 21:33:04 -0400

    Reorganized constants and common functions, fixed TeleOp, and updated Autonomous

---
+++ constants.h
@@ -0,0 +1,40 @@
+//Flag for whether to use tank drive or not
+//#define TANKDRIVE
+//#define LOGTARGET
+
+//Scaling
+#define JOYSTICKHIGH 100
+#define JOYSTICKLOW 30
+#define SHOULDERHIGH 50
+#define SHOULDERLOW 20
+#define SHOULDERDOWNHIGH 10
+#define SHOULDERDOWNLOW 5
+#define ARMHIGH 30
+#define ARMLOW 15
+#define ARMDOWNHIGH 5
+#define ARMDOWNLOW 3
+//Robot Constants
+#define SHOULDERTOP 2100
+#define SHOULDERBOTTOM 0
+#define SHOULDERUPRIGHT 2100
+#define ARMTOP 1800
+#define ARMBOTTOM -800
+#define ARMUPRIGHT 64
+#define HANDMAX 300
+#define HANDMIN 0
+//Field Constants
+#define RINGPEGSHOULDER 410
+#define RINGPEGARM 320
+#define BOTTOMPEG 280
+#define MIDDLEPEG 0
+#define TOPPEG 1440
+
+//Autonomous
+#define FORWARD 5800
+#define TURN 700
+#define CENTERFORWARD 2700
+#define SIDEWAYS 1800
+#define PLACEFORWARD 500
+#define BACKWARDS 1440
+#define TURNTORINGS 1200
+#define FORWARDTORINGS 2400
