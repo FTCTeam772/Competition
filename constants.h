844417448ab761954b98da76643db97adf2fa384
Foster McLane <fkmclane@gmail.com>
2013-10-09 20:05:01 -0400

    Added arm protection (uncalibrated)

--- constants.h
+++ constants.h
@@ -4,8 +4,14 @@
 //Scaling
 #define DRIVE_HIGH 100
 #define DRIVE_LOW 30
-#define ARM_HIGH 30 // 40
+#define ARM_HIGH 30
 #define ARM_LOW 10
+
+//Robot Constants
+#define ARM_SHOULDER_TOP 1000
+#define ARM_SHOULDER_BOTTOM 0
+#define ARM_ELBOW_TOP 1000
+#define ARM_ELBOW_BOTTOM 0
 
 //Testing
 #define TEST_ENCODER 4800
