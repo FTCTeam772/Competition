083d0dac45f676a9bfc3c099332bd351a18f5fbc
Foster McLane <fkmclane@gmail.com>
2013-11-13 19:15:04 -0500

    Added hand code

--- constants.h
+++ constants.h
@@ -1,17 +1,20 @@
 //Flags
-//#define NONLINEARTARGET
+#define NONLINEARTARGET
 
 //Scaling
 #define DRIVE_HIGH 100
 #define DRIVE_LOW 30
-#define ARM_HIGH 40
-#define ARM_LOW 10
+#define ARM_HIGH 100
+#define ARM_LOW 30
+#define HAND_HIGH 20
 
 //Robot Constants
 #define ARM_SHOULDER_TOP 1000
 #define ARM_SHOULDER_BOTTOM 0
 #define ARM_ELBOW_TOP 1000
 #define ARM_ELBOW_BOTTOM 0
+#define HAND_MAX 1400
+#define HAND_MIN 0
 
 //Testing
 #define TEST_ENCODER 4800
