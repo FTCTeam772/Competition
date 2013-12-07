a15a3b065190e66ee9e0cdb8e8ce7380cedf832e
Foster McLane <fkmclane@gmail.com>
2013-12-07 17:05:32 -0500

    Added arm presets and some calibration

--- constants.h
+++ constants.h
@@ -6,15 +6,20 @@
 #define DRIVE_LOW 30
 #define ARM_HIGH 100
 #define ARM_LOW 30
-#define HAND_HIGH 20
+#define HAND_HIGH 50
 
 //Robot Constants
-#define ARM_SHOULDER_TOP 1000
+#define ARM_SHOULDER_TOP 28000
 #define ARM_SHOULDER_BOTTOM 0
-#define ARM_ELBOW_TOP 1000
-#define ARM_ELBOW_BOTTOM 0
 #define HAND_MAX 1400
 #define HAND_MIN 0
+
+//Presets
+#define ENCODER_PRECISION 30
+#define ARM_SHOULDER_PICKUP 26600
+#define ARM_ELBOW_PICKUP 1800
+#define ARM_SHOULDER_HANG 10000
+#define ARM_ELBOW_HANG 2000
 
 //Testing
 #define TEST_ENCODER 4800
