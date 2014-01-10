d879cca2dda71f3cae98a9a77ce6a06ad9ef2d44
Foster McLane <fkmclane@gmail.com>
2014-01-10 13:39:55 -0500

    Forgot to negate one variable in rewiring

--- constants.h
+++ constants.h
@@ -39,10 +39,10 @@
 #define WAIT 30
 //Autonomous Programs 0 and 2
 #define AUTO_DETECT 10400
-#define AUTO_IR_CORRECT -1300
+#define AUTO_IR_CORRECT -1500
 #define AUTO_IR_TURN 900
-#define AUTO_RAMP_1 2800
-#define AUTO_RAMP_2 6000
+#define AUTO_RAMP_1 1300
+#define AUTO_RAMP_2 6100
 #define AUTO_RAMP_TURN 900
 #define AUTO_RAMP_UP 3700
 //Autonomous Programs 1 and 3
