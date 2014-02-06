de1d783c317d61ac1720105ef10bb74c77071019
Foster McLane <fkmclane@gmail.com>
2014-02-05 21:46:07 -0500

    Update version number

--- TeleOp.c
+++ TeleOp.c
@@ -183,7 +183,7 @@ task main() {
 	nMotorEncoder[FrontLeft] = nMotorEncoder[FrontRight] = nMotorEncoder[BackLeft] = nMotorEncoder[BackRight] = nMotorEncoder[LeftArmShoulder] = nMotorEncoder[LeftArmElbow] = nMotorEncoder[RightArmShoulder] = nMotorEncoder[RightArmElbow] = nMotorEncoder[LeftHand] = nMotorEncoder[RightHand] = 0; // Might as well reset the encoders too
 
 	//Display the robot's name
-	nxtDisplayCenteredTextLine(0, "Codex 3.14");
+	nxtDisplayCenteredTextLine(0, "Codex 3.14159");
 
 	//Go time!
 	waitForStart();
