331e2ecfdd525526f826bf7283636185ddfc3fd5
Foster McLane <fkmclane@gmail.com>
2014-02-08 19:24:22 -0500

    Player program searches for .mus files to play

--- Player.c
+++ Player.c
@@ -7,21 +7,29 @@
 
 #include "Player.h"
 
-string songs[] = { "StillAlive.mus", "Mario.mus" };
-string names[] = { "Still Alive", "Super Mario Bros." };
-
 task main() {
-	display = true;
+	display = true; //Show music controls
 
-	TFileHandle file;
+	TFileHandle file; //For saving info about each file
 	TFileIOResult result;
-	char * filename;
+	string filename;
 	int size;
 
-	nxtDisplayCenteredTextLine(3, "Playing:");
-	nxtDisplayCenteredTextLine(4, names[0]);
-	setSong(songs[0]);
-	wait10Msec(150);
-	play();
-	while(playing && nNxtButtonPressed != 4);
+	nxtDisplayCenteredTextLine(3, "Select a song:");
+	FindFirstFile(file, result, "*.mus", filename, size); //Find the first file
+	nxtDisplayCenteredTextLine(5, "%s", filename); //And display its name
+	while(nNxtButtonPressed != 3) { //While center button is not pressed to select a song
+		wait10Msec(20);
+		if(nNxtButtonPressed == 1 || nNxtButtonPressed == 2) {
+			FindNextFile(file, result, filename, size); //Find the next file
+			if(result != ioRsltSuccess) //Unless we hit the end
+				FindFirstFile(file, result, "*.mus", filename, size); //Then find the first file again
+			nxtDisplayCenteredTextLine(5, "%s", filename); //Display the filename
+		}
+	}
+	while(nNxtButtonPressed == 3); //Wait for unpress
+
+	setSong(filename); //Set the song
+	play(); //And play it
+	while(playing); //Wait until the end
 }
