1d7b30560a7cf2a8d0d82d86d327c67bd84fac34
Foster McLane <fkmclane@gmail.com>
2014-03-21 20:15:45 -0400

    Fixed building music on Windows

--- Music/make.bat
+++ Music/make.bat
@@ -1,4 +1,4 @@
 @echo off
-for %file in (Files\*.mus) do (
-  Compiler\win32\MusicCompiler.exe %file NXT\%~nI.nms
+for %%f in (Files\*.mus) do (
+  Compiler\win32\MusicCompiler.exe %%f NXT\%%~nf.nms
 )
