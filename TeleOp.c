e4d1d9057030948f4ee7c494e80fe10d1b9f8653
Adam Blocker <nextel16@embarqmail.com>
2014-10-30 11:52:55 -0400

    Got rid of last year's flag & arm stuff

--- TeleOp.c
+++ TeleOp.c
@@ -3,7 +3,6 @@
 #pragma config(Sensor, S3,     IR,             sensorHiTechnicIRSeeker1200)
 #pragma config(Motor,  motorA,          RightHand,     tmotorNXT, PIDControl, reversed, encoder)
 #pragma config(Motor,  motorB,          LeftHand,      tmotorNXT, PIDControl, reversed, encoder)
-#pragma config(Motor,  motorC,          Flag,          tmotorNXT, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C1_1,     FrontRight,    tmotorTetrix, PIDControl, reversed, encoder)
 #pragma config(Motor,  mtr_S1_C1_2,     BackRight,     tmotorTetrix, PIDControl, reversed, encoder)
 #pragma config(Motor,  mtr_S1_C2_1,     FrontLeft,     tmotorTetrix, PIDControl, encoder)
@@ -63,25 +62,10 @@ task slideControl() {
 		float x1, y1, x2, y2;
 
 		//Check each axis for deadband
-//		if((joystick.joy2_x1 > DEADBAND || joystick.joy2_x1 < -DEADBAND) && abs(joystick.joy2_x1) > abs(joystick.joy2_y1))
-//			x1 = joystick.joy2_x1 / 128.0;
-//		else
-//			x1 = 0;
-
 		if((joystick.joy2_y1 > DEADBAND || joystick.joy2_y1 < -DEADBAND) && abs(joystick.joy2_y1) > abs(joystick.joy2_x1))
 			y1 = joystick.joy2_y1 / 128.0;
 		else
 			y1 = 0;
-
-//		if((joystick.joy2_x2 > DEADBAND || joystick.joy2_x2 < -DEADBAND) && abs(joystick.joy2_x2) > abs(joystick.joy2_y2))
-//			x2 = joystick.joy2_x2 / 128.0;
-//		else
-//			x2 = 0;
-
-//		if((joystick.joy2_y2 > DEADBAND || joystick.joy2_y2 < -DEADBAND) && abs(joystick.joy2_y2) > abs(joystick.joy2_x2))
-//			y2 = joystick.joy2_y2 / 128.0;
-//		else
-//			y2 = 0;
 
 		//Protect arms unless button 5 is pressed
 		if(((x1 < 0 && nMotorEncoder[LeftSlide] <= SLIDE_BOTTOM) || (x1 > 0 && nMotorEncoder[LeftSlide] >= SLIDE_TOP)) && !joy2Btn(5))
@@ -94,8 +78,6 @@ task slideControl() {
 
 		//Set the motors to scale
 		motor[LeftSlide] = motor[RightSlide] = slide_scale * y1;
-//		motor[LeftArmElbow] = arm_scale * x1;
-//		motor[RightArmElbow] = arm_scale * x2;
 
 		//If button 7 is pressed, open the left hand
 		if(joy2Btn(7))
@@ -115,55 +97,8 @@ task slideControl() {
 
 		//writeDebugStream("Hands:\n\tLeft Hand:\t%d\n\tRight Hand:\t%d\n", nMotorEncoder[LeftHand], nMotorEncoder[RightHand]);
 
-		//Presets
-
-		//Left arm pickup
-/*		if(joy2Btn(1)) {
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-			while((abs(nMotorEncoder[LeftArmShoulder] - ARM_SHOULDER_PICKUP) > ENCODER_PRECISION || abs(nMotorEncoder[LeftArmElbow] - ARM_ELBOW_PICKUP) > ENCODER_PRECISION) && !joy2Btn(5)) {
-				motor[LeftArmShoulder] = targetMotorSpeed(ARM_SHOULDER_PICKUP, nMotorEncoder[LeftArmShoulder]) * ARM_HIGH;
-				motor[LeftArmElbow] = targetMotorSpeed(ARM_ELBOW_PICKUP, nMotorEncoder[LeftArmElbow]) * ARM_LOW;
-				//writeDebugStream("LeftArm:\t%d\t%d\n", nMotorEncoder[LeftArmShoulder], nMotorEncoder[LeftArmElbow]);
-			}
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-		}
-
-		//Home
-		if(joy2Btn(2)) {
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-			while((nMotorEncoder[LeftArmShoulder] > ENCODER_PRECISION || nMotorEncoder[LeftArmElbow] > ENCODER_PRECISION || nMotorEncoder[RightArmShoulder] > ENCODER_PRECISION || nMotorEncoder[RightArmElbow] > ENCODER_PRECISION) && !joy2Btn(5)) {
-				motor[LeftArmShoulder] = targetMotorSpeed(0, nMotorEncoder[LeftArmShoulder]) * ARM_HIGH;
-				motor[LeftArmElbow] = targetMotorSpeed(0, nMotorEncoder[LeftArmElbow]) * ARM_LOW;
-				motor[RightArmShoulder] = targetMotorSpeed(0, nMotorEncoder[RightArmShoulder]) * ARM_HIGH;
-				motor[RightArmElbow] = targetMotorSpeed(0, nMotorEncoder[RightArmElbow]) * ARM_LOW;
-				//writeDebugStream("LeftArm:\t%d\t%d\nRightArm:\t%d\t%d\n", nMotorEncoder[LeftArmShoulder], nMotorEncoder[LeftArmElbow], nMotorEncoder[RightArmShoulder], nMotorEncoder[RightArmElbow]);
-			}
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-		}
-
-		//Right arm pickup
-		if(joy2Btn(3)) {
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-			while((abs(nMotorEncoder[RightArmShoulder] - ARM_SHOULDER_PICKUP) > ENCODER_PRECISION || abs(nMotorEncoder[RightArmElbow] - ARM_ELBOW_PICKUP) > ENCODER_PRECISION) && !joy2Btn(5)) {
-				motor[RightArmShoulder] = targetMotorSpeed(ARM_SHOULDER_PICKUP, nMotorEncoder[RightArmShoulder]) * ARM_HIGH;
-				motor[RightArmElbow] = targetMotorSpeed(ARM_ELBOW_PICKUP, nMotorEncoder[RightArmElbow]) * ARM_LOW;
-				//writeDebugStream("RightArm:\t%d\t%d\n", nMotorEncoder[RightArmShoulder], nMotorEncoder[RightArmElbow]);
-			}
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-		}
-
-		//Hanging
-		if(joy2Btn(4)) {
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-			while((abs(nMotorEncoder[LeftArmShoulder] - ARM_SHOULDER_HANG) > ENCODER_PRECISION || abs(nMotorEncoder[LeftArmElbow] - ARM_ELBOW_HANG) > ENCODER_PRECISION || abs(nMotorEncoder[RightArmShoulder] - ARM_SHOULDER_HANG) > ENCODER_PRECISION || abs(nMotorEncoder[RightArmElbow] - ARM_ELBOW_HANG) > ENCODER_PRECISION) && !joy2Btn(5)) {
-				motor[LeftArmShoulder] = targetMotorSpeed(ARM_SHOULDER_HANG, nMotorEncoder[LeftArmShoulder]) * ARM_HIGH;
-				motor[LeftArmElbow] = targetMotorSpeed(ARM_ELBOW_HANG, nMotorEncoder[LeftArmElbow]) * ARM_LOW;
-				motor[RightArmShoulder] = targetMotorSpeed(ARM_SHOULDER_HANG, nMotorEncoder[RightArmShoulder]) * ARM_HIGH;
-				motor[RightArmElbow] = targetMotorSpeed(ARM_ELBOW_HANG, nMotorEncoder[RightArmElbow]) * ARM_LOW;
-				//writeDebugStream("LeftArm:\t%d\t%d\nRightArm:\t%d\t%d\n", nMotorEncoder[LeftArmShoulder], nMotorEncoder[LeftArmElbow], nMotorEncoder[RightArmShoulder], nMotorEncoder[RightArmElbow]);
-			}
-			motor[LeftArmShoulder] = motor[LeftArmElbow] = motor[RightArmShoulder] = motor[RightArmElbow] = motor[LeftHand] = motor[RightHand] = 0;
-		}*/
+		//Preset Positions
+			
 	}
 }
 
@@ -183,11 +118,6 @@ task main() {
 			drive_scale = DRIVE_LOW;
 		else //Else be full speed
 			drive_scale = DRIVE_HIGH;
-
-		if(joy1Btn(2)) //If the driver is pressing button 2, spin the flag turner
-			motor[Flag] = 100;
-		else
-			motor[Flag] = 0;
 
 		switch(joystick.joy1_TopHat) { //Play a song based on the D-pad value
 			case 0:
