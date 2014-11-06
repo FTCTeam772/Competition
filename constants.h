148650244f96ab6fc56b4e7a7eac6496a7cfbc24
Adam Blocker <nextel16@embarqmail.com>
2014-11-06 10:01:28 -0500

    No need for last year's constants

--- constants.h
+++ constants.h
@@ -6,37 +6,21 @@
 
 //Scaling
 #define DRIVE_HIGH 100
-#define DRIVE_LOW 30
-#define ARM_HIGH 100
-#define ARM_LOW 30
-#define HAND_HIGH 50
+#define DRIVE_LOW 40
+#define SLIDE_HIGH 100
+#define SLIDE_LOW 40
 
 //Robot Constants
 #define ENCODER_PRECISION 60
-#define ARM_SHOULDER_TOP 28000
-#define ARM_SHOULDER_BOTTOM 0
-#define HAND_MAX 150
-#define HAND_MIN 0
+#define SLIDE_TOP 28000       //Needs to be calibrated
+#define SLIDE_BOTTOM 0
 
 //Presets
-#define ARM_SHOULDER_PICKUP 27000
-#define ARM_ELBOW_PICKUP 2200
-#define ARM_SHOULDER_HANG 12500
-#define ARM_ELBOW_HANG -700
-#define ARM_SHOULDER_BASKET 14200
-#define ARM_ELBOW_BASKET -1200
 
 //Autonomous
 #define WAIT 30
+
 //IR Autonomous Programs
-#define AUTO_DETECT 10400
-#define AUTO_IR_CORRECT -1300
-#define AUTO_IR_TURN 900
-#define AUTO_RAMP_1 1300
-#define AUTO_RAMP_2 6100
-#define AUTO_RAMP_TURN 900
-#define AUTO_RAMP_UP 3700
+
 //Non-IR Autonomous Programs
-#define AUTO_FIRST_BASKET 2411
-#define AUTO_RAMP 2000
-#define AUTO_RAMP_OVER 6400
+
