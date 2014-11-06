1d4e38a164992e47ae0c126a66f722d62684629f
Adam Blocker <nextel16@embarqmail.com>
2014-11-06 10:27:28 -0500

    Added goals. Not sure if left/right will differ

--- constants.h
+++ constants.h
@@ -16,11 +16,15 @@
 #define SLIDE_BOTTOM 0
 
 //Presets
-
+#define LEFT_LOW_GOAL 2000
+#define RIGHT_LOW_GOAL 2000
+#define LEFT_MEDIUM_GOAL 4000
+#define RIGHT_MEDIUM_GOAL 4000
+#define LEFT_HIGH_GOAL 6000
+#define RIGHT_HIGH_GOAL 6000
 //Autonomous
 #define WAIT 30
 
 //IR Autonomous Programs
 
 //Non-IR Autonomous Programs
-
