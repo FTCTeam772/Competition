369e97d3e2bdc7c8cfb4128fbe6ff4d172483071
Adam Blocker <nextel16@embarqmail.com>
2014-11-17 19:20:36 -0500

    Added turn and updateAngle methods

--- common.h
+++ common.h
@@ -8,20 +8,20 @@ void initialize() {
 }
 
 float targetMotorSpeed(int target, int current) {
-#ifdef NONLINEARTARGET
+	#ifdef NONLINEARTARGET
 	if(abs(current - target) > 400)
 		return sgn(target - current);
 	else if(abs(current - target) > ENCODER_PRECISION)
 		return 2 * pow(cos(PI / 4 * (current - target + 400) / 400), 2) - 1;
 	else
 		return 0;
-#else
-	return (abs(current - target) > ENCODER_PRECISION) ? 1 : 0;
-#endif
+	#else
+		return (abs(current - target) > ENCODER_PRECISION) ? 1 : 0;
+	#endif
 }
 
 double updateXpos(double angle, double Xpos, double dist) {
-	Xpos += (dist * cos(angle)); 
+	Xpos += (dist * cos(angle));
 	return Xpos;
 
 }
@@ -33,18 +33,33 @@ double updateYpos (double angle, double Ypos, double dist) {
 }
 
 void move(double angle, double Xpos, double Ypos, double dist) {
-		nMotorEncoder[FrontLeft] = nMotorEncoder[FrontRight] = nMotorEncoder[BackLeft] = nMotorEncoder[BackRight];
-		
-		while(nMotorEncoder[FrontLeft] <= dist && nMotorEncoder[FrontRight] <= dist && nMotorEncoder[BackLeft] <= dist && nMotorEncoder[BackRight] <= dist) {
-		motor[FrontLeft] = motor[FrontRight] = motor[BackLeft] = motor [BackRight] = DRIVE_HIGH;
+		nMotorEncoder[FrontLeft] = nMotorEncoder[FrontRight] = nMotorEncoder[BackLeft] = nMotorEncoder[BackRight] = 0;
+
+		while(abs(nMotorEncoder[FrontLeft] - dist) > ENCODER_PRECISION || abs(nMotorEncoder[FrontRight] - dist) > ENCODER_PRECISION || abs(nMotorEncoder[BackLeft] - dist) > ENCODER_PRECISION || abs(nMotorEncoder[BackRight] - dist) > ENCODER_PRECISION) {
+			motor[FrontLeft] = motor[FrontRight] = motor[BackLeft] = motor [BackRight] = DRIVE_HIGH * ANDYMARK_CONVERSION * dist/(abs(dist));
 		}
-		
+
 		motor[FrontLeft] = motor[FrontRight] = motor[BackLeft] = motor [BackRight] = 0;
 		updateXpos(angle, Xpos, dist);
 		updateYpos(angle, Ypos, dist);
 }
 
+void turn(double angle){
+		while(abs(angle - SensorValue[Compass]) > ANGLE_PRECISION){
+			if(angle - SensorValue[Compass] > ANGLE_PRECISION){		//turn left
+				motor[FrontLeft] = motor[BackLeft] = DRIVE_HIGH * ANDYMARK_CONVERSION * -1;
+				motor[FrontRight] = motor[BackRight] = DRIVE_HIGH * ANDYMARK_CONVERSION;
+			}
+			else {		//turn right
+				motor[FrontLeft] = motor[BackLeft] = DRIVE_HIGH * ANDYMARK_CONVERSION;
+				motor[FrontRight] = motor[BackRight] = DRIVE_HIGH * ANDYMARK_CONVERSION * -1;
+			}
+	}
+}
 
+double updateAngle (double angleOld, double angleNew){  //returns
+	return angleNew - angleOld;
+}
 
 void wait() {
 	wait10Msec(WAIT);
