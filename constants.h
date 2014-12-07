6d8cffa9eac99555de92576ebc6f42ff2369d397
Adam Blocker <nextel16@embarqmail.com>
2014-12-06 20:20:07 -0500

    deleted line/update

--- constants.h
+++ constants.h
@@ -4,7 +4,6 @@
 //Controller constants
 #define DEADBAND 15
 
-
 //Scaling
 #define DRIVE_HIGH 100
 #define DRIVE_LOW 40
@@ -18,18 +17,17 @@
 #define SLIDE_TOP 28000       //Needs to be calibrated
 #define SLIDE_BOTTOM 0
 #define CONT_SERVO_CENTER 128
-#define ENCODERCONVERSION 4.0; //degree to encoder value conversion
 
 //Presets (all must be calibrated)
-#define LOW_GOAL 7000
-#define MEDIUM_GOAL 14000
-#define HIGH_GOAL 21000
-#define CENTER_GOAL 28000
+#define LOW_GOAL 5000
+#define MEDIUM_GOAL 10000
+#define HIGH_GOAL 15000
+#define CENTER_GOAL 20000
 //Autonomous
 #define WAIT 30
-#define ZIPTIE_WAIT 100
-#define RAMP true    //Will the robot start on the ramp?
-#define DEF true     //Will the robot do defense?
+#define ZIPTIE_WAIT 120
+#define RAMP false    //Will the robot start on the ramp?
+#define DEF false     //Will the robot do defense?
 #define KICK true    //Will the robot knock down the kick stand?
-#define CENTER true  //Will the robot go for the center goal?
+#define CENTER false  //Will the robot go for the center goal?
 #define ROLLING 0    //Which rolling goal will the robot go for?  (0 = no rolling goals, 1 = medium goal only, 2 = high goal only, 3 = both goals)
