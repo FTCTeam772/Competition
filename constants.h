a7f03e68839354e887ab5f22732970a657abf1df
Adam Blocker <nextel16@embarqmail.com>
2014-12-13 14:53:02 -0500

    constants for exam

--- constants.h
+++ constants.h
@@ -14,19 +14,19 @@
 #define ENCODER_PRECISION 80
 #define ANGLE_PRECISION 3
 #define ANDYMARK_CONVERSION -78.0/100.0
-#define SLIDE_TOP 8000
+#define SLIDE_TOP 7000
 #define SLIDE_BOTTOM 0
 #define CONT_SERVO_CENTER 128
 
 //Presets
 #define LOW_GOAL 2000
-#define MEDIUM_GOAL 4350
+#define MEDIUM_GOAL 4400
 #define HIGH_GOAL 6500
 #define CENTER_GOAL 7000
 //Autonomous
 #define WAIT 30
-#define ZIPTIE_WAIT 150
-#define RAMP false    //Will the robot start on the ramp?
+#define ZIPTIE_WAIT 170
+#define RAMP true    //Will the robot start on the ramp?
 #define DEF false     //Will the robot do defense?
 #define KICK true    //Will the robot knock down the kick stand?
 #define CENTER false  //Will the robot go for the center goal?
