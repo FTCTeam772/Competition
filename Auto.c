7aee113a418056a7f5062858a03a567a910eb543
Adam Blocker <nextel16@embarqmail.com>
2015-01-14 20:26:56 -0500

    Ziptie tweak

--- Auto.c
+++ Auto.c
@@ -29,157 +29,236 @@
 //Music player
 #include "Player.h"
 
-void kickCenter(bool kick, bool center){
+//Initialize our globals
+byte drive_scale = DRIVE_HIGH; 		//Used to scale down robot movements
+byte slide_scale = SLIDE_HIGH;
 
-		//Get IR Values
-		int irvalue = -1;
+//Declare large constants as longs
+task driveControl() { //Asynchronous task for critical drive control
+	while(true) {
+		//Joystick 1 - Driver
 
-		if ((SensorValue[IR_left] == 5 && SensorValue[IR_right] == 6) || (SensorValue[IR_left] == 5 && SensorValue[IR_right] == 0)) { //center goal is facing the box
-			irvalue = 0;
+		//Deadband between -DEADBAND to DEADBAND
+		//Scale linearly for all other values
+		float y1, y2;
+
+		//writeDebugStream("IR sensor:\t%d\n", SensorValue[IR]);
+		writeDebugStream("LeftSlide:\t%d\n", nMotorEncoder[LeftSlide]);
+ 		writeDebugStream("RightSlide:\t%d\n", nMotorEncoder[RightSlide]);
+
+		//Tank Drive
+		if(joystick.joy1_y1 > DEADBAND || joystick.joy1_y1 < -DEADBAND)		//Left stick controls left wheels
+			y1 = joystick.joy1_y1 / 128.0;
+		else
+			y1 = 0;
+
+		if(joystick.joy1_y2 > DEADBAND || joystick.joy1_y2 < -DEADBAND)		//Right stick controls right wheels
+			y2 = joystick.joy1_y2 / 128.0;
+		else
+			y2 = 0;
+
+		//Set the motors to scale
+		motor[LeftWheels] = drive_scale * y1 * ANDYMARK_CONVERSION;
+		motor[BackRight] = motor[FrontRight] = drive_scale * y2 * ANDYMARK_CONVERSION;
+
+		//writeDebugStream("Wheels:\n\tFront Left:\t%d\n\tFront Right:\t%d\n\tBack Left:\t%d\n\tBack Right:\t%d\n", nMotorEncoder[FrontLeft], nMotorEncoder[FrontRight], nMotorEncoder[BackLeft], nMotorEncoder[BackRight]);
+	}
+}
+
+task slideControl() {
+	while(true) {
+		//Joystick 2 - Operator
+
+		float y1;
+		//Check each axis for deadband
+		if(joystick.joy2_y1 > DEADBAND || joystick.joy2_y1 < -DEADBAND){
+			y1 = joystick.joy2_y1 / 128.0;
+		}
+		else {
+			y1 = 0;
 		}
 
-		if (SensorValue[IR_left] == 6 && SensorValue[IR_right] == 7) { //center goal is facing at a 45 degree angle between the box and side of the ramp
-			irvalue = 1;
+		//Protect slides unless button 5 is pressed
+		//if(((y1 < 0 && nMotorEncoder[LeftSlide] <= SLIDE_BOTTOM) || (y1 < 0 && nMotorEncoder[RightSlide] <= SLIDE_BOTTOM) || (y1 > 0 && nMotorEncoder[LeftSlide] >= SLIDE_TOP) || (y1 > 0 && nMotorEncoder[RightSlide] >= SLIDE_TOP)) && !joy2Btn(5))
+		//	y1 = 0;
+
+		if(((y1 < 0 && nMotorEncoder[RightSlide] <= SLIDE_BOTTOM) || (y1 > 0 && nMotorEncoder[RightSlide] >= SLIDE_TOP)) && !joy2Btn(5))
+			y1 = 0;
+
+		//writeDebugStream("Slides:\n\tLeft Slide:\t%d\n\tRight Slide:\t%d\n", nMotorEncoder[LeftSlide], nMotorEncoder[RightSlide]);
+
+		//Set the motors to scale
+		if(joystick.joy2_TopHat == 0) { //Hold the lift up
+			motor[LeftSlide] = BUMP;
+			motor[RightSlide] = BUMP;
 		}
-
-		if (SensorValue[IR_left] == 5 && SensorValue[IR_right] == 5) { //center goal is facing toward the side of the ramp
-			irvalue = 2;
+		else if(joy2Btn(9)){		//If button nine is pressed, only move the left side
+			motor[LeftSlide] = slide_scale * y1 * ANDYMARK_CONVERSION;
+			motor[RightSlide] = 0;
 		}
+		else if(joy2Btn(10)){		//If button ten is pressed, only move the right side
+			motor[LeftSlide] = 0;
+			motor[RightSlide] = slide_scale * y1 * ANDYMARK_CONVERSION;
+		}
+		else {		//Else move both
+			motor[LeftSlide] = motor[RightSlide] = slide_scale * y1 * ANDYMARK_CONVERSION;
+		}
+		//Preset Positions
 
-		if (center == true) {
-			//run kickstand method for ramp start
-
-		if (irvalue == 0) { //center goal is facing the box
-			//score in the centergoal
-
-			if (kick == true){
-				//knock over kickstand
-
+		//Home
+		if(joy2Btn(2)) {
+			motor[LeftSlide] = motor[RightSlide] = 0;
+			while((nMotorEncoder[RightSlide] > ENCODER_PRECISION) && !joy2Btn(5)){ 		//nMotorEncoder[LeftSlide] > ENCODER_PRECISION ||
+				motor[LeftSlide] = -SLIDE_HIGH * ANDYMARK_CONVERSION;		//targetMotorSpeed(0, nMotorEncoder[LeftSlide]) *
+				motor[RightSlide] = -SLIDE_HIGH * ANDYMARK_CONVERSION;			//targetMotorSpeed(0, nMotorEncoder[RightSlide]) *
+				//writeDebugStream("LeftSlide:\t%d\t%d\RightSlide:\t%d\t%d\n", nMotorEncoder[LeftSlide], nMotorEncoder[RightSlide]);
 			}
+			motor[LeftSlide] = motor[RightSlide] = 0;
 		}
 
-		if (irvalue == 1) { //center goal is facing at a 45 degree angle between the box and side of the ramp
-			//score in the centergoal
-
-			if (kick == true){
-				//knock over kickstand
-
+		//Low Rolling Goal
+		if(joy2Btn(1)) {
+			motor[LeftSlide] = motor[RightSlide] = 0;
+			while((abs(nMotorEncoder[RightSlide] - LOW_GOAL) > ENCODER_PRECISION) && !joy2Btn(5)) {		//abs(nMotorEncoder[LeftSlide] - LOW_GOAL) > ENCODER_PRECISION ||
+				motor[LeftSlide] = targetMotorSpeed(LOW_GOAL, nMotorEncoder[LeftSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				motor[RightSlide] = targetMotorSpeed(LOW_GOAL, nMotorEncoder[RightSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				//writeDebugStream("LeftSlide:\t%d\t%d\RightSlide:\t%d\t%d\n", nMotorEncoder[LeftSlide], nMotorEncoder[RightSlide]);
 			}
+			motor[LeftSlide] = motor[RightSlide] = 0;
 		}
 
-		if (irvalue == 2) { //center goal is facing toward the side of the ramp
-			//score in the centergoal
-
-			if (kick == true){
-				//knock over kickstand
-
+		//Medium Rolling Goal
+		if(joy2Btn(3)) {
+			motor[LeftSlide] = motor[RightSlide] = 0;
+			while((abs(nMotorEncoder[RightSlide] - MEDIUM_GOAL) > ENCODER_PRECISION) && !joy2Btn(5)) {		//(abs(nMotorEncoder[LeftSlide] - MEDIUM_GOAL) > ENCODER_PRECISION ||
+				motor[LeftSlide] = targetMotorSpeed(MEDIUM_GOAL, nMotorEncoder[LeftSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				motor[RightSlide] = targetMotorSpeed(MEDIUM_GOAL, nMotorEncoder[RightSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				//writeDebugStream("LeftSlide:\t%d\t%d\RightSlide:\t%d\t%d\n", nMotorEncoder[LeftSlide], nMotorEncoder[RightSlide]);
 			}
+			motor[LeftSlide] = motor[RightSlide] = 0;
+		}
+
+		//High Rolling Goal
+		if(joy2Btn(4)) {
+			motor[LeftSlide] = motor[RightSlide] = 0;
+			while((abs(nMotorEncoder[RightSlide] - HIGH_GOAL) > ENCODER_PRECISION) && !joy2Btn(5)) {		//(abs(nMotorEncoder[LeftSlide] - HIGH_GOAL) > ENCODER_PRECISION ||
+				motor[LeftSlide] = targetMotorSpeed(HIGH_GOAL, nMotorEncoder[LeftSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				motor[RightSlide] = targetMotorSpeed(HIGH_GOAL, nMotorEncoder[RightSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				//writeDebugStream("LeftSlide:\t%d\t%d\RightSlide:\t%d\t%d\n", nMotorEncoder[LeftSlide], nMotorEncoder[RightSlide]);
+			}
+			motor[LeftSlide] = motor[RightSlide] = 0;
+		}
+
+		//Center Goal
+		if(joy2Btn(7)) {
+			motor[LeftSlide] = motor[RightSlide] = 0;
+			while((abs(nMotorEncoder[RightSlide] - CENTER_GOAL) > ENCODER_PRECISION) && !joy2Btn(5)) {		//(abs(nMotorEncoder[LeftSlide] - CENTER_GOAL) > ENCODER_PRECISION ||
+				motor[LeftSlide] = targetMotorSpeed(CENTER_GOAL, nMotorEncoder[LeftSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				motor[RightSlide] = targetMotorSpeed(CENTER_GOAL, nMotorEncoder[RightSlide]) * SLIDE_HIGH * ANDYMARK_CONVERSION;
+				//writeDebugStream("LeftSlide:\t%d\t%d\RightSlide:\t%d\t%d\n", nMotorEncoder[LeftSlide], nMotorEncoder[RightSlide]);
+			}
+			motor[LeftSlide] = motor[RightSlide] = 0;
 		}
 	}
-
-		if (kick == true){		//center is false & kickstand is true
-
-			if (irvalue == 0) { //center goal is facing the box
-				//knock over kickstand
-				oneSideTurn(-1900, false);
-				oneSideTurn(-1700, true);
-				drive(-6000);
-			}
-
-			if (irvalue == 1) { //center goal is facing at a 45 degree angle
-				//knock over kickstand
-				turn(3000);
-				drive(3100);
-				turn(-2300);
-				drive(-7000);
-			}
-
-			if (irvalue == 2) { //center goal is facing toward the side of the ramp
-				//knock over kickstand
-				drive(-3500);
-				turn(-3000);
-				drive(-3000);
-				drive(2000);
-				drive(-4000);
-		  }
-		}
-	}
-
-
-void execute(bool ramp, bool def, bool kick, bool center, int roll) {			//I feel like this should be outside of task main()
-
-		if (ramp == true) {			//If starting from ramp
-
-			if (roll == 1) {
-				//score in medium goal and bring it back (bring it back can be a common.h method) to parking zone
-				drive(11550);
-				setGrabbers();
-				liftScore(MEDIUM_GOAL);
-				grabGoal();
-				turn(1200);
-				drive(-10700);
-				turn(6200);
-				releaseGoal();
-				}
-
-			kickCenter(kick, center);
-		}
-
-		if (ramp == false) {
-
-			if (def == true) {
-				//run defense program for floor beginning
-			}
-			if (def == false) {
-
-				if (roll == 0) {		//If not scoring in rolling goals
-					drive(-2400);
-				}
-
-				if (roll == 1) {
-					//score in medium goal and bring it back
-					drive(1000);
-					turn(1050);
-					drive(12000);
-					liftScore(MEDIUM_GOAL);
-					grabGoal();
-					wait();
-					drive(-9000);
-					turn(6000);
-					releaseGoal();
-				}
-				if (roll == 2) {
-					//score in tallest goal and bring it back
-					drive(1000);
-					turn(970);
-					drive(14500);
-					liftScore(HIGH_GOAL);
-					grabGoal();
-					wait();
-					drive(-12000);
-					turn(6000);
-					releaseGoal();
-				}
-			
-				kickCenter(kick, center);
-			}
-		}
-	}
+}
 
 task main() {
-
 	//Initialize
 	initialize();
 
 	//Go time!
 	waitForStart();
 
-	//Play the Imperial March
-	//setSong("ImperialMarch.nms");
-	//play();
+	StartTask(driveControl); //Go ahead and start critical drive functions in their own task
+	StartTask(slideControl); //Start arm functions
 
-	//wait10Msec(delay * 100);
+	while(true) {
+		//Joystick 1 - Driver
 
- 	execute(RAMP, DEF, KICK, CENTER, ROLLING);
+		if(joy1Btn(6)) 		//If the driver is pressing button 6, slow down movements
+			drive_scale = DRIVE_LOW;
+		else 		//Else be full speed
+			drive_scale = DRIVE_HIGH;
 
+		switch(joystick.joy1_TopHat) { //Play a song based on the D-pad value
+			case 0:
+				setSong("1Up.nms");
+				play();
+				break;
+			case 2:
+				setSong("SuperMario.nms");
+				play();
+				break;
+			case 4:
+				setSong("CastleComplete.nms");
+				play();
+				break;
+			case 6:
+				setSong("Starman.nms");
+				play();
+				break;
+		}
+
+		if(joy1Btn(4)) { //If the driver is pressing button 4, play the Imperial March
+			setSong("ImperialMarch.nms");
+			play();
+		}
+		if(joy1Btn(9)) { //If the driver is pressing button 9, play Still Alive
+			setSong("StillAlive.nms");
+			play();
+		}
+		if(joy1Btn(10)) { //If the driver is pressing button 10, stop song
+			stop();
+		}
+
+		//Goal Grabbers
+		int leftpos = 0;		//Keeps track of the position of the left servo
+		int rightpos = 0;		//Keeps track of the position of the right servo
+
+		if(joy1Btn(2)) {		//Both grabbers go into the down position
+			servo[leftGrab] = LEFT_GRAB_DOWN;
+			servo[rightGrab] = RIGHT_GRAB_DOWN;
+			leftpos = 1;
+			rightpos = 1;
+		}
+		if(joy1Btn(4)) {		//Both grabbers go into the up position
+			servo[leftGrab] = LEFT_GRAB_UP;
+			servo[rightGrab] = LEFT_GRAB_DOWN;
+			leftpos = 0;
+			rightpos = 0;
+		}
+		if(joy1Btn(1) && leftpos == 0) {		//Left grabber goes into the down position
+			servo[leftGrab] = LEFT_GRAB_DOWN;
+			leftpos = 1;
+   	}
+   	if(joy1Btn(1) && leftpos == 1) {		//Left grabber goes into the up position
+    	servo[leftGrab] = LEFT_GRAB_UP;
+    	leftpos = 0;
+  	}
+  	if(joy1Btn(3) && rightpos == 0) {		//Right grabber goes into the down position
+    	servo[rightGrab] = RIGHT_GRAB_DOWN;
+    	rightpos = 1;
+    }
+  	if(joy1Btn(3) && rightpos == 1) {		//Right grabber goes into the up position
+   		servo[rightGrab] = RIGHT_GRAB_UP;
+   		rightpos = 0;
+  	}
+
+  	//Joystick 2 - Operator
+		if(joy2Btn(6)) //If the operator is pressing button 6, scale down the slide movements
+			slide_scale = SLIDE_LOW;
+		else
+			slide_scale = SLIDE_HIGH;
+
+		//Zip Ties
+		if(joystick.joy2_y2 > DEADBAND)		//If the operator is putting in a value on the x-axis of the right stick
+			motor[zipties] = 85;					//Make the servo spin accordingly
+		else if(joystick.joy2_y2 < -DEADBAND)
+			motor[zipties] = -20;
+		else
+			motor[zipties] = 0;					//If within DEADBAND, remain stopped
+
+		writeDebugStream("IR_left:\t%d\n", SensorValue[IR_left]);
+		writeDebugStream("IR_right:\t%d\n" ,SensorValue[IR_right]);
+	}
 }
