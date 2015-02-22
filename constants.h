272de9155970b774b2d01f3abee8400472069529
Adam Blocker <nextel16@embarqmail.com>
2015-02-21 20:31:53 -0500

    SLIDE_LOW & LEFT_GRAB_UP

--- constants.h
+++ constants.h
@@ -8,7 +8,7 @@
 #define DRIVE_HIGH 100
 #define DRIVE_LOW 40
 #define SLIDE_HIGH -100
-#define SLIDE_LOW -50
+#define SLIDE_LOW -20
 #define BUMP 15
 
 //Robot Constants
@@ -24,7 +24,7 @@
 #define MEDIUM_GOAL 4600
 #define HIGH_GOAL 6700
 #define CENTER_GOAL 8500
-#define LEFT_GRAB_UP 220
+#define LEFT_GRAB_UP 250
 #define RIGHT_GRAB_UP 0
 #define LEFT_GRAB_DOWN 45
 #define RIGHT_GRAB_DOWN 180
@@ -33,8 +33,8 @@
 #define DELAY 0     //Delay at beginning of autonomous
 #define WAIT 30       //Used in wait()
 #define ZIPTIE_WAIT 170
-#define RAMP false    //Will the robot start on the ramp?
-#define DEF true     //Will the robot play defence?
+#define RAMP true    //Will the robot start on the ramp?
+#define DEF false     //Will the robot play defence?
 #define KICK false    //Will the robot knock down the kick stand?
 #define CENTER false  //Will the robot attempt to score in center goal?
-#define ROLLING 0    //Which rolling goal will the robot go for?  (0 = no rolling goals, 1 = medium goal only, 2 = high goal only)
+#define ROLLING 1    //Which rolling goal will the robot go for?  (0 = no rolling goals, 1 = medium goal only, 2 = high goal only)
